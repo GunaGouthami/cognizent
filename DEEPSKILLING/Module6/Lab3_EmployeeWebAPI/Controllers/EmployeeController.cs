989878a55713dd86ec6104dabd29d4df7bd00dad
GunaGouthami <gowthamiguna3@gmail.com>
2026-07-18 20:59:45 +0530

    Update EmployeeController.cs

--- DEEPSKILLING/Module6/Lab3_EmployeeWebAPI/Controllers/EmployeeController.cs
+++ DEEPSKILLING/Module6/Lab3_EmployeeWebAPI/Controllers/EmployeeController.cs
@@ -1,1 +1,75 @@
+using EmployeeApiDemo.Filters;
+using EmployeeApiDemo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
+namespace EmployeeApiDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [CustomAuthFilter]
+    public class EmployeeController : ControllerBase
+    {
+        private List<Employee> GetStandardEmployeeList()
+        {
+            return new List<Employee>()
+            {
+                new Employee
+                {
+                    Id = 1,
+                    Name = "John",
+                    Salary = 50000,
+                    Permanent = true,
+                    DateOfBirth = new DateTime(1998,1,1),
+                    Department = new Department
+                    {
+                        Id = 1,
+                        Name = "IT"
+                    },
+                    Skills = new List<Skill>
+                    {
+                        new Skill{ Id = 1, Name = "C#" },
+                        new Skill{ Id = 2, Name = "SQL" }
+                    }
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "David",
+                    Salary = 45000,
+                    Permanent = false,
+                    DateOfBirth = new DateTime(1999,5,10),
+                    Department = new Department
+                    {
+                        Id = 2,
+                        Name = "HR"
+                    },
+                    Skills = new List<Skill>
+                    {
+                        new Skill{ Id = 3, Name = "Java" }
+                    }
+                }
+            };
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<List<Employee>> GetStandard()
+        {
+            return Ok(GetStandardEmployeeList());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Employee employee)
+        {
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Employee employee)
+        {
+            return Ok(employee);
+        }
+    }
+}
