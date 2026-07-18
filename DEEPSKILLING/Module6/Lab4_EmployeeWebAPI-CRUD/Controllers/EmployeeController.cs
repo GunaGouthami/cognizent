511f86ad6021befef3e3822d0761692a7edf254e
GunaGouthami <gowthamiguna3@gmail.com>
2026-07-18 21:41:11 +0530

    Update EmployeeController.cs

--- DEEPSKILLING/Module6/Lab4_EmployeeWebAPI-CRUD/Controllers/EmployeeController.cs
+++ DEEPSKILLING/Module6/Lab4_EmployeeWebAPI-CRUD/Controllers/EmployeeController.cs
@@ -1,1 +1,69 @@
+using EmployeeCrudApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace EmployeeCrudApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private static List<Employee> employees = new List<Employee>()
+        {
+            new Employee
+            {
+                Id = 1,
+                Name = "John",
+                Salary = 50000,
+                Permanent = true
+            },
+            new Employee
+            {
+                Id = 2,
+                Name = "David",
+                Salary = 60000,
+                Permanent = false
+            }
+        };
+
+        // GET
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<List<Employee>> Get()
+        {
+            return Ok(employees);
+        }
+
+        // POST
+        [HttpPost]
+        public ActionResult<Employee> Post([FromBody] Employee employee)
+        {
+            employees.Add(employee);
+            return Ok(employee);
+        }
+
+        // PUT
+        [HttpPut]
+        public ActionResult<Employee> Put([FromBody] Employee employee)
+        {
+            if (employee.Id <= 0)
+            {
+                return BadRequest("Invalid employee id");
+            }
+
+            Employee existingEmployee = employees.FirstOrDefault(e => e.Id == employee.Id);
+
+            if (existingEmployee == null)
+            {
+                return BadRequest("Invalid employee id");
+            }
+
+            existingEmployee.Name = employee.Name;
+            existingEmployee.Salary = employee.Salary;
+            existingEmployee.Permanent = employee.Permanent;
+
+            return Ok(existingEmployee);
+        }
+    }
+}
